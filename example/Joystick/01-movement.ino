3b80af4ed3e301e7e6a67075eee3635fdb4e8831
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-11-30 15:50:02 +0700

    Create 01-movement.ino

---
+++ example/Joystick/01-movement.ino
@@ -0,0 +1,36 @@
+#include "MatrixMiniR4.h"
+
+int l_speed;
+int r_speed;
+int ly;
+int rx;
+
+void setup()
+{
+  MiniR4.begin();
+  MiniR4.PWR.setBattCell(2);
+  Serial.begin(9600);
+  //MiniR4.M3.setReverse(false);
+  //MiniR4.M4.setReverse(true);
+  MiniR4.M3.setSpeed(0);
+  MiniR4.M4.setSpeed(0);
+}
+
+void loop()
+{
+  MiniR4.PS2.read_gamepad(false, 0);
+  
+  ly = MiniR4.PS2.Analog(PSS_LY);
+  rx = MiniR4.PS2.Analog(PSS_RX);
+
+  ly = map(ly,0,255,-100,100);
+  rx = map(rx,0,255,-100,100);
+  Serial.print(ly);
+  Serial.println(rx);
+
+  l_speed = ly+rx;
+  r_speed = rx-ly;
+
+  MiniR4.M4.setSpeed(l_speed);
+  MiniR4.M3.setSpeed(r_speed);
+}
