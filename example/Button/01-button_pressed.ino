1567b9b3758bb6b3ac51c533dba0f6f99dca275e
Teeraphat Kullanankanjana <watit2013@gmail.com>
2025-01-01 16:55:16 +0700

    Get Button State
    Get Button State

---
+++ example/Button/01-button_pressed.ino
@@ -0,0 +1,21 @@
+#include "MatrixMiniR4.h"
+
+void setup()
+{
+  MiniR4.begin();
+  MiniR4.PWR.setBattCell(2);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(MiniR4.BTN_UP.getState())
+  {
+    Serial.println("Button Up is pressed!");
+  }
+  if(MiniR4.BTN_DOWN.getState())
+  {
+    Serial.println("Button Down is pressed!");
+  }
+
+}
